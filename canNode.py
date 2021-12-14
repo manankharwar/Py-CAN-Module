31dd59894807f3ade76667798c06312d61353e4b
Jeremy Langner <langnerj@mcmaster.ca>
2021-12-14 12:40:27 -0500

    added python can module with basic init parameters

--- canNode.py
+++ canNode.py
@@ -1,8 +1,30 @@
+import can
+from can.interface import Bus
 
 class canNode:
+    bus = Bus()
 
-    def __init__(self):
-        pass
+    '''
+        interface: String -> interface that represents the CAN interface specified
+        channed: String -> CAN channel to be used
+        bitrate: Int -> bitrate for CAN communication
+    '''
+    def __init__(self, interface, channel, bitrate):
+        try:
+            can.rc['interface'] = interface
+        except:
+            print("Please enter a correct interface")
+
+        try: 
+            can.rc['channel'] = channel
+        except:
+            print("Please enter a correct channel")
+
+        try:
+            can.rc['bitrate'] = bitrate
+        except:
+            print("Please enter a valid bitrate")
+        
 
     def send(self):
         pass
