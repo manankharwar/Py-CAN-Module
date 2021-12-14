b7e3bb0f4ec00c1daee9e0a8b2caa73f77c6c37d
Jeremy Langner <langnerj@mcmaster.ca>
2021-12-14 12:16:07 -0500

    Updated string

--- canString.py
+++ canString.py
@@ -1,3 +1,10 @@
+
+
+'''
+    This is a class to represent the different types of CAN
+    string messages that canNode will utilize to 
+    communicate with other hardware.
+'''
 class canString:
 
     def __init__(self):
