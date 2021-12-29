4fd5e274136f9c5b9de3c7e249687ed7b1b58249
Jeremy Langner <langnerj@mcmaster.ca>
2021-12-29 11:07:33 -0500

    removed dictionaries, renamed some constants

--- constants.py
+++ constants.py
@@ -19,58 +19,27 @@ driverInputs = [0x64, 48]
 vehicleDynamics = [0x2BC, 40]
 
 
-dictDriver = {
-    1: channel_Driver.DashboardAccumulatorData,
-    2: channel_Driver.DashboardMotorData,
-    3: channel_Driver.DashboardVehicleData,
-    4: channel_Driver.DriverWheelControls,
-    5: channel_Driver.DriverDriverSettings,
-    6: channel_Driver.DriverDrivingInputs,
-    7: channel_Driver.DriverVehicleDynamics
-}
-
 # Cooling channel
 controlWords = [0x1000, 48]
 statusWords = [0x1100, 40]
 acknowledgement = [0x1200, 1]
 
-dictCooling = {
-    1: channel_Cooling.CoolingControlWords,
-    2: channel_Cooling.CoolingStatusWords,
-    3: channel_Cooling.CoolingAcknowledgement
-}
-
 
 # AMK channel
-amkSetpoints1Inverter0 = [0x184, 64]
-amkSetpoints1Inverter1 = [0x194, 64]
-amkSetpoints1Inverter2 = [0x1A4, 64]
-amkSetpoints1Inverter3 = [0x1B4, 64]
 
-amkActualValues1Inverter0 = [0x283, 64]
-amkActualValues2Inverter0 = [0x285, 64]
-amkActualValues1Inverter1 = [0x293, 64]
-amkActualValues2Inverter1 = [0x295, 64]
-amkActualValues1Inverter2 = [0x2A3, 64]
-amkActualValues2Inverter2 = [0x2A5, 64]
-amkActualValues1Inverter3 = [0x2B3, 64]
-amkActualValues2Inverter3 = [0x2B5, 64]
+#amk channel constants for description: SetPoints_Inverter_
+amkSP1I0 = [0x184, 64]
+amkSP1I1 = [0x194, 64]
+amkSP1I2 = [0x1A4, 64]
+amkSP1I3 = [0x1B4, 64]
 
+#amk channel constants for description: ActualValues_Inverter_
+amkAV1I0 = [0x283, 64]
+amkAV2I0 = [0x285, 64]
+amkAV1I1 = [0x293, 64]
+amkAV2I1 = [0x295, 64]
+amkAV1I2 = [0x2A3, 64]
+amkAV2I2 = [0x2A5, 64]
+amkAV1I3 = [0x2B3, 64]
+amkAV2I3 = [0x2B5, 64]
 
-#BUG with dictAMK... apparently there is a circular import although I cannot find it
-'''
-dictAMK = {
-    1: channel_AMK.AMKSetPoints1Inverter0,
-    2: channel_AMK.AMKSetPoints1Inverter1,
-    3: channel_AMK.AMKSetPoints1Inverter2,
-    4: channel_AMK.AMKSetPoints1Inverter3,
-    5: channel_AMK.AMKActualValues1Inverter0,
-    6: channel_AMK.AMKActualValues2Inverter0,
-    7: channel_AMK.AMKActualValues1Inverter1,
-    8: channel_AMK.AMKActualValues2Inverter1,
-    9: channel_AMK.AMKActualValues1Inverter2,
-    10: channel_AMK.AMKActualValues2Inverter2,
-    11: channel_AMK.AMKActualValues1Inverter3,
-    12: channel_AMK.AMKActualValues2Inverter3
-}
-'''
